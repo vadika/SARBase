6cf00562e4408d6a35e7887a6d4ebd36e70f309b
vadik likholetov <vadikas@gmail.com>
2023-11-16 10:59:39 +0200

    Initial internationalisation implementation

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_babel import Babel
 from sqlalchemy import MetaData
+
 
 convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -15,13 +17,23 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 
+def get_locale():
+    return session.get('language', request.accept_languages.best_match(['en', 'ru', 'et', 'lv', 'fi']))
+
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://sarbaseuser:password@localhost/sarbaseapp'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+babel= Babel(app)
+babel.init_app(app, locale_selector=get_locale)
 db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+
+
 
 import models
 import admin
@@ -33,6 +45,14 @@ import dashboard
 @app.route('/')
 def hello_world():  # put application's code here
     return  redirect(url_for('dashboard'))
+
+
+@app.route('/set_language/<lang_code>')
+def set_language(lang_code):
+    session['language'] = lang_code
+    return redirect(request.referrer or url_for('dashboard'))
+
+
 
 
 if __name__ == '__main__':
